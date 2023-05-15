feab83556a78c72469b5558b7815c024fe9ac84e
kaleckh <69163050+kaleckh@users.noreply.github.com>
2023-05-15 09:42:23 -0600

    first commit

---
+++ src/app/pages/main.js
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+import styles from './styles/page.module.css'
+
+export default function main() {
+    return (
+        <div className={styles.main}>
+            kaleck
+        </div>
+    )
+}
